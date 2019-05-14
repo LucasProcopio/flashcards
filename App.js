c2b41698dec0bd83fd19f9b5f9f094ea7638abd4
Lucas Henrique Procopio <procmode.dev@gmail.com>
2019-05-13 22:38:28 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default class App extends React.Component {
+  render () {
+    // TODO: create an TAB navigation,
+    // TODO: cerate an add deck view
+
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
